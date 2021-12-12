8e0dedf7bc2f4a6a906c2bc08aecaf8d2406c754
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2021-12-12 13:49:19 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -8,6 +8,7 @@ logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
+from yt_dlp import YoutubeDL, DownloadError
 import asyncio
 import json
 import math
