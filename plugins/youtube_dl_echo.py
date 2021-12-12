2fcd337417b720e7f6fb1f91d1ef57b160549c91
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2021-12-12 13:40:27 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -91,6 +91,9 @@ async def echo(bot, update):
             "-j",
             url
         ]
+    if "zee5" in url:
+        command_to_exec.append("--geo-bypass-country")
+        command_to_exec.append("IN")
     if youtube_dl_username is not None:
         command_to_exec.append("--username")
         command_to_exec.append(youtube_dl_username)
